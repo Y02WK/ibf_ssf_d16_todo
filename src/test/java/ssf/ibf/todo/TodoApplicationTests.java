21faa1c24a54113b6a6e406ca3df5077c8c8ad52
Y02WK <59220086+Y02WK@users.noreply.github.com>
2022-01-12 09:30:53 +0800

    Initial commit

---
+++ src/test/java/ssf/ibf/todo/TodoApplicationTests.java
@@ -0,0 +1,13 @@
+package ssf.ibf.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
